5ddc99751fff454ca64180ab6107e6da7af34b7b
Abhinav Khetarpal <35734172+abhinavk@users.noreply.github.com>
2019-04-05 15:44:18 +0530

    Update data_extraction.py

--- data_gathering_preprocessing/data_extraction.py
+++ data_gathering_preprocessing/data_extraction.py
@@ -1,1 +1,12 @@
+"""
+@author: Abhinav Khetarpal
+"""
+import search_twitter
 
+tweet_data = search_twitter.search_for_tweets('IndianElection2014', 10, 201404011200, 201405121200).json()
+
+texts = []
+
+for result in tweet_data['results']:
+    print(result['text'])
+    texts.append(result['text'])
