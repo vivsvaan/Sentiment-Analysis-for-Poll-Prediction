c36a67ffc63f5400cb2a4859f2581e0905318e3a
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-04-05 16:34:16 +0530

    updated

--- data_gathering_preprocessing/data_extraction.py
+++ data_gathering_preprocessing/data_extraction.py
@@ -1,12 +1,32 @@
-"""
-@author: Abhinav Khetarpal
-"""
-import search_twitter
+# """
+# @author: Abhinav Khetarpal
+# """
+# import search_twitter
 
-tweet_data = search_twitter.search_for_tweets('IndianElection2014', 10, 201404011200, 201405121200).json()
+# tweet_data = search_twitter.search_for_tweets('IndianElection2014', 10, 201404011200, 201405121200).json()
 
-texts = []
+# texts = []
 
-for result in tweet_data['results']:
-    print(result['text'])
-    texts.append(result['text'])
+# for result in tweet_data['results']:
+#     print(result['text'])
+#     texts.append(result['text'])
+
+ 
+import csv
+
+text = ['text', ' ']
+comp = ['comp', ' ']
+pos = ['pos', ' ']
+neg = ['neg', ' ']
+neut = ['neutral', ' ']
+
+for _ in range(10):
+    text.append("ab")
+
+
+
+with open('data.csv', 'w', newline='') as outfile:
+    rowlists = zip(text, comp, pos, neg, neut)
+    spamwriter = csv.writer(outfile)
+    for row in rowlists:
+        spamwriter.writerow(row)
