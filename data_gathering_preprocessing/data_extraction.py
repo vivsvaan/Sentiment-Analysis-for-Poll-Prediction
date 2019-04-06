56e5195e6203f5aeb33f52b3be9f49a9649f1bd6
Ashlin-Siby <36127896+Ashlin-Siby@users.noreply.github.com>
2019-04-07 01:53:20 +0530

    Added function to store tweets in CSV Format

--- data_gathering_preprocessing/data_extraction.py
+++ data_gathering_preprocessing/data_extraction.py
@@ -1,28 +1,28 @@
-# """
-# @author: Abhinav Khetarpal
-# """
-# import search_twitter
+import search_twitter
 
-# tweet_data = search_twitter.search_for_tweets('IndianElection2014', 10, 201404011200, 201405121200).json()
+hashtags2019 =['bjp','modi','ModiLaoDeshBanao','hindu','ModiShah','NarendraModi','namo','amitshah','maibhichowkidar','chowkidarNarendraModi','ModiHaiToMumkinHai']
+startdate2019 = ['201903011200','201903151200','201903291200']
+endate2019=['201903141200','201903281200','201904061200']
 
-# texts = []
+hashtags2014 =['bjp','modi','ModiLaoDeshBanao','WeWantModi','AbKiBaarModiSarkaar','NarendraModi','namo','GharGharModi','AcheDinAaneWaleHai','RGForEducation','RGForIndia','RGTheBest','congress','rahulgandhi']
+startdate2014 = ['201404011200','201404151200','201404291200']
+endate2014=['201404141200','201404281200','201405121200']
 
-# for result in tweet_data['results']:
-#     print(result['text'])
-#     texts.append(result['text'])
+tweets = []
+hashs = []
 
- 
+for (sdate,edate) in zip(startdate2014,endate2014):
+    for hash in hashtags2019:
+        tweet_data = search_twitter.search_for_tweets(hash, 99,sdate,edate).json()
+        for result in tweet_data['results']:
+            hashs.append(hash)
+            tweets.append(result['text'])
+
+
+#print(text) 
 import csv
-
-text = ['text', ' ']
-comp = ['comp', ' ']
-pos = ['pos', ' ']
-neg = ['neg', ' ']
-neut = ['neutral', ' ']
-
-
-with open('data.csv', 'w', newline='') as outfile:
-    rowlists = zip(text, comp, pos, neg, neut)
+with open('data_2019.csv', 'a',encoding="utf-8", newline='') as outfile:
+    rowlists = zip(hashs,tweets)
     spamwriter = csv.writer(outfile)
     for row in rowlists:
-        spamwriter.writerow(row)
+          spamwriter.writerow(row)
