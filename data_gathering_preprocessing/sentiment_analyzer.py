78bd6bb881ab19e59e6baed6f1a9bbbd499a5117
kamal-aujla <35269583+kamal-aujla@users.noreply.github.com>
2019-04-07 10:04:52 +0530

    Added sentiment analysis over tweets

--- data_gathering_preprocessing/sentiment_analyzer.py
+++ data_gathering_preprocessing/sentiment_analyzer.py
@@ -1,6 +1,8 @@
 """
 @author: Abhinav Khetarpal
 """
+import numpy as np
+import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # TODO: For each tweet text in the collection, add the values for - pos, neg, neu, compound under appropriate attribute in table.
@@ -17,11 +19,22 @@ def analyze(text):
 
     return sentiment
 
+ 
+#input from data_new.csv files    
+dataset = pd.read_csv('data_new.csv')
+X = dataset.iloc[:, 1:2].values #input tweets in form of numpy ndarray
+text = X.tolist() #changing numpy ndarray to list
+l = len(text)
+
+sentiment_list = []  #list to store sentiment analysis result for all tweets
+
+for i in range(0,l):
+    temp = text[i][0]  #accessing content of list text one by one
+    sentiment = analyze(temp)
+    sentiment_list.append(sentiment)  #storing result of sentiment analysis in sentiment_list
 
 
-# Sample code, delete when not required.
-text = 'I wanna be a billionaire so fucking bad.'
+#name of the variables can be changed according to convinience
+#Store the Conent of sentiment_list in excel or csv
 
-sentiment = analyze(text)
-print(sentiment)
 
