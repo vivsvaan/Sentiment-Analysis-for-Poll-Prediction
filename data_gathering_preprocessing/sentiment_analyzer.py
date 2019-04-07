4ae3d972a99284e7a40b70d23337d31f8c55541b
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-04-07 13:53:03 +0530

    sentiment analyzer working now

--- data_gathering_preprocessing/sentiment_analyzer.py
+++ data_gathering_preprocessing/sentiment_analyzer.py
@@ -1,40 +1,52 @@
 """
-@author: Abhinav Khetarpal
+@author: Abhinav Khetarpal, Vivsvaan Sharma
 """
 import numpy as np
 import pandas as pd
+import csv
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# TODO: For each tweet text in the collection, add the values for - pos, neg, neu, compound under appropriate attribute in table.
 
-# import this file and then use in your code.
-def analyze(text):
+def analyze(tweet):
     """
     Takes text to be analyzed as input
     :return: a dictionary with the scores for sentiments - pos, neg, neu, compound
     """
     analyzer = SentimentIntensityAnalyzer()
     
-    sentiment = analyzer.polarity_scores(text)
+    sentiment = analyzer.polarity_scores(tweet)
 
     return sentiment
 
  
-#input from data_new.csv files    
-dataset = pd.read_csv('data_new.csv')
-X = dataset.iloc[:, 1:2].values #input tweets in form of numpy ndarray
-text = X.tolist() #changing numpy ndarray to list
-l = len(text)
+#input from cleaned_data_2019.csv files    
+dataset = pd.read_csv('cleaned_data_2019.csv')
+hashtags = dataset.iloc[:, 0].values
+X = dataset.iloc[:, 1].values #don't reshape array X
+all_tweets = list(X) #changing numpy array to list
 
-sentiment_list = []  #list to store sentiment analysis result for all tweets
+hashtags = list(hashtags)
+neg = ["neg"]
+neu = ["neu"]
+pos = ["pos"]
+compound = ["compound"]
 
-for i in range(0,l):
-    temp = text[i][0]  #accessing content of list text one by one
-    sentiment = analyze(temp)
-    sentiment_list.append(sentiment)  #storing result of sentiment analysis in sentiment_list
 
+for i in range(0,len(all_tweets)):
+    tweet = all_tweets[i]  #accessing content of list all_tweets one by one
+    sentiment = analyze(tweet)
+    #adding all values in their respective lists
+    neg.append(sentiment['neg'])
+    neu.append(sentiment['neu'])
+    pos.append(sentiment['pos'])
+    compound.append(sentiment['compound'])
 
-#name of the variables can be changed according to convinience
-#Store the Conent of sentiment_list in excel or csv
-
+#Storing sentiment values along with hashtags and tweets in sentiment_data_2019.csv file
+hashtags[:] = ["hashtags"] + hashtags[:]
+all_tweets[:] = ["tweets"] + all_tweets[:]
+with open('sentiment_data_2019.csv', 'a',encoding="utf-8", newline='') as outfile:
+    rowlists = zip(hashtags,all_tweets, neg, neu, pos, compound)
+    spamwriter = csv.writer(outfile)
+    for row in rowlists:
+          spamwriter.writerow(row)
 
