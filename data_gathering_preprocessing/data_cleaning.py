126b6271b0218d16ffccc3d64d6d4b534edf97cf
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-04-12 11:06:40 +0530

    updates cleaning file

--- data_gathering_preprocessing/data_cleaning.py
+++ data_gathering_preprocessing/data_cleaning.py
@@ -17,21 +17,48 @@ def text_cleaning(text):
     return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
 
 
-dataset = pd.read_csv('data_2019.csv')
+dataset = pd.read_csv('2014_new_congress.csv')
+
+#fetching data from csv file
 hashtags = dataset.iloc[:,0].values
-X = dataset.iloc[:, 1].values
-#X = X.reshape((X.shape[0], 1))
-hashs = list(hashtags)
-tweets = list(X)
+dates= dataset.iloc[:,1].values
+tweets = dataset.iloc[:,2].values
+location = dataset.iloc[:,3].values
+followers= dataset.iloc[:,4].values
+fav = dataset.iloc[:,5].values
+reply = dataset.iloc[:,6].values
+retweet = dataset.iloc[:,7].values
+
+#converting nparray to lists
+hashtags = list(hashtags)
+dates = list(dates)
+tweets = list(tweets)
+location = list(location)
+followers = list(followers)
+fav = list(fav)
+reply = list(reply)
+retweet = list(retweet)
 cleaned_tweets = []
 
+#cleaning tweets
 for text in tweets:
     cleaned_text = text_cleaning(text)
     cleaned_text = " ".join(cleaned_text)
     cleaned_tweets.append(cleaned_text)
 
-with open('cleaned_data_2019.csv', 'a',encoding="utf-8", newline='') as outfile:
-    rowlists = zip(hashs,cleaned_tweets)
+#adding headings
+hashtags = ['hashtags'] + hashtags
+dates = ['dates'] + dates
+cleaned_tweets = ['cleaned_tweets'] + cleaned_tweets
+location = ['location'] + location
+followers = ['followers'] + followers
+fav = ['fav'] + fav
+reply = ['reply'] + reply
+retweet = ['retweet'] + retweet
+
+#storing in csv file
+with open('2014_new_congress_cleaned.csv', 'a',encoding="utf-8", newline='') as outfile:
+    rowlists = zip(hashtags,dates,cleaned_tweets,location,followers,fav,reply,retweet)
     spamwriter = csv.writer(outfile)
     for row in rowlists:
           spamwriter.writerow(row)
